4d3d252189b99d14dc799c264fe5daac89fb8891
chaupt <chaupt@webvanta.com>
2011-01-23 15:43:47 -0800

    We got it all working using Webvanta and loading the story automatically from the web site. This let's us edit the story page by page if needed.

--- story.js
+++ story.js
@@ -1,49 +1,30 @@
 $(document).ready(function() {
 
-  var story = [
-    { page: 1,
-      title: "So it begins...",
-      plot: "You are in a maze of twisty-turny passages all a like. There is a fork ahead",
-      choice_1: "I go left, into the dark",
-      choice_2: "I go right, towards the light",
-      score_1: 10, score_2: 1,
-      next_1: 2, next_2: 4 },
-
-    { page: 2,
-      title: "Darkness...",
-      plot: "Wow, it is really dark. You stub your toe. You are in a maze of twisty-turny passages all a like. There is a fork ahead",
-      choice_1: "I rub my toe and head towards the light",
-      choice_2: "I kick that darn rock, and head towards the light",
-      score_1: 1, score_2: 5,
-      next_1: 5, next_2: 3 },
-    
-    { page: 3,
-      title: "Darkness in pain...",
-      plot: "Your toe really hurts, you are limping, and you come across an underground lake.",
-      choice_1: "I dive into the water and swim towards an sparkling light",
-      choice_2: "I walk around the lake and take a path leading up",
-      score_1: 10, score_2: 1,
-      next_1: 4, next_2: 5 },
-    
-    { page: 4,
-      title: "Glimmers...",
-      plot: "You enter a room that sparkles with shiny crystals.",
-      choice_1: "I walk to the exit",
-      choice_2: "I pick up some crystals, put them in my pocket, and head out",
-      score_1: 1, score_2: 10,
-      next_1: 5, next_2: 5 },
-      
-    { page: 5,
-      title: "So it ends...",
-      plot: "Somehow, you got out",
-      end: true }
-      
-  ]
-
+  var story = [];
   var score = 0;
   var choices = [];
   
+  var after_data_is_loaded = function(data) {
+    story = data;
+    $("#loading").hide(); 
+    $("#story-container").show(); 
+    story_engine(1);
+  }
+  
+  var load_data_and_start = function() {
+    $.ajax({
+      url: "/story_data.json",
+      cache: false,
+      dataType: 'json',
+      error: function(x,s,e) {alert("got error"+s);}, 
+      success: function(data){after_data_is_loaded(data);}
+    });
+  };
+  
   var save_report_results = function() {
+    var name = $("#test_results_results_name").val();
+    if (!name || name.length < 1)
+      $("#test_results_results_name").val('Mystery Visitor');
     $("#test_results_dymd_score").val(score);
   };
   
@@ -61,25 +42,34 @@ $(document).ready(function() {
   
   var story_engine = function(next_page) {
     var current_page = get_next_page(next_page);
-    if (current_page.end != true) {
-      var page_markup = "<h3>" + current_page.title + "</h3><p>" + current_page.plot + "</p><ol>" +
+    if (current_page.end) {
+      var page_markup = "<p>" + current_page.plot + "</p><button id='end_button'>Finish Test</button>";      
+      
+      $("#story-container").html(page_markup);
+      $("#end_button").click(function(){$('form').submit();}); 
+      
+      save_report_results();      
+        
+    } else if (current_page.bridge) {
+      var page_markup = "<p>" + current_page.plot + "</p><ol>" +
+      "<li><a id='bridge'>Continue...</a></li>" +
+      "</ol>";
+      
+      $("#story-container").html(page_markup);
+      $("#bridge").click(function(){story_engine(current_page.bridge)});
+      
+    } else {
+      var page_markup = "<h3>" + (current_page.title || '') + "</h3><p>" + current_page.plot + "</p><ol>" +
       "<li><a id='choice_1'>" + current_page.choice_1 + "</a></li>" +
       "<li><a id='choice_2'>" + current_page.choice_2 + "</a></li>" +
       "</ol>";
       
       $("#story-container").html(page_markup);
-      $("#choice_1").click(function(){score = score + current_page.score_1; story_engine(current_page.next_1)});
-      $("#choice_2").click(function(){score = score + current_page.score_2; story_engine(current_page.next_2)});
-      
-    } else {
-      var page_markup = "<h3>" + current_page.title + "</h3><p>" + current_page.plot + "</p><button id='end_button'>Finish Test</button>";      
-      $("#story-container").html(page_markup);
-      $("#end_button").click(function(){$('form').submit();});   
-      save_report_results();
-      
+      $("#choice_1").click(function(){score = score + parseInt(current_page.score_1); story_engine(current_page.next_1)});
+      $("#choice_2").click(function(){score = score + parseInt(current_page.score_2); story_engine(current_page.next_2)});      
     }
   };
  
-  $('#start_button').click(function(){$('form').hide(); $("#story-container").show(); story_engine(1); return false;});
+  $('#start_button').click(function(){$('form').hide(); $('#loading').show(); load_data_and_start(); return false;});
 }
 )
